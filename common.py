52ae3c2c0df88daebbcdaa5f83adceb6e78356cc
nagyrobert1990 <nagy.robert1990@gmailcom>
2017-11-30 13:11:51 +0100

    id generator in common.py is completed

--- common.py
+++ common.py
@@ -1,7 +1,7 @@
 # implement commonly used functions here
 
-import random
-
+from random import randint
+import string
 
 # generate and return a unique and random string
 # other expectations:
@@ -25,4 +25,31 @@ def generate_random(table):
 
     # your code
 
-    return generated
+    list_lowercase = string.ascii_lowercase
+    list_uppercase = string.ascii_uppercase
+    list_numbers = string.digits
+    list_misc = ["!","@","#","$","%","^","&","*","(",")","?"]
+    
+    while True :
+
+        while len(generated)<8:
+            choose = randint(0,3)
+            generated = generated + list_lowercase[randint(0,len(list_lowercase)-1)] + list_uppercase[randint(0,len(list_uppercase)-1)] + list_numbers[randint(0,len(list_numbers)-1)] + list_misc[randint(0,len(list_misc)-1)]
+            if choose == 0:
+                randomchar = randint(0,len(list_lowercase)-1)
+                generated = generated + list_lowercase[randomchar]
+            elif choose == 1:
+                randomchar = randint(0,len(list_uppercase)-1)
+                generated = generated + list_uppercase[randomchar]
+            elif choose == 2:
+                randomchar = randint(0,len(list_numbers)-1)
+                generated = generated + list_numbers[randomchar]
+            else:
+                randomchar = randint(0,len(list_misc)-1)
+                generated = generated + list_misc[randomchar]
+
+        for i in range(len(table)):
+
+            if table[i][0] != generated:
+
+                return generated
