b504b36b371ecdead2e27fda292a358825ced34b
nagyrobert1990 <nagy.robert1990@gmailcom>
2017-12-12 15:02:24 +0100

    hr.py special functions are completed

--- hr/hr.py
+++ hr/hr.py
@@ -8,129 +8,44 @@ def start_module():
 
     common.submenu_of_managements(3)
 
-'''
-def id_existance(table,id_):
 
-    for i in range(len(table)):
-        if table[i][0] == id_ :
-            return True
-    
-    return False
-
-def show_table(table):
-    """
-    Display a table
-
-    Args:
-        table: list of lists to be displayed.
-
-    Returns:
-        None
-    """
-
-    # your code
-
-    ui.print_table(table,['ID','Name','Birth Date'])
-
-    pass
-
-
-def add(table):
-    """
-    Asks user for input and adds it into the table.
-
-    Args:
-        table: table to add new record to
-
-    Returns:
-        Table with a new record
-    """
-
-    # your code
-
-    new_row = ui.get_inputs(['Name: ','Birth Date: '],"Please, give me the specs.")
-    added_id = common.generate_random(table)
-    new_row.insert(0,added_id)
-    table.append(new_row)
-    data_manager.write_table_to_file("hr/persons.csv",table)
-    show_table(table)
-    ui.print_error_message('The specs are added with ID: ' + added_id)
-
-    return table
-
-
-def remove(table, id_):
-    """
-    Remove a record with a given id from the table.
-
-    Args:
-        table: table to remove a record from
-        id_ (str): id of a record to be removed
-
-    Returns:
-        Table without specified record.
-    """
-
-    # your code
-
-    for i in range(len(table)):
-        if table[i][0] == id_:
-            del(table[i])
-            break
-    
-    data_manager.write_table_to_file("hr/persons.csv",table)
-    show_table(table)
-    ui.print_error_message('The row with ID: %s has been removed.' % id_)
-
-    return table
-
-
-def update(table, id_):
-    """
-    Updates specified record in the table. Ask users for new data.
-
-    Args:
-        table: list in which record should be updated
-        id_ (str): id of a record to update
-
-    Returns:
-        table with updated record
-    """
-
-    # your code
-
-    updated_row = ui.get_inputs(['Name: ','Birth Date: '],"Please, give me the specs.")
-    updated_row.insert(0,id_)
-    
-    for i in range(len(table)):
-        if table[i][0] == id_:
-            table.insert(i,updated_row)
-            del(table[i+1])
-            break
-
-    data_manager.write_table_to_file("hr/persons.csv",table)
-    show_table(table)
-    ui.print_error_message('The row with ID: %s has been updated.' % id_)
-
-    return table
-'''
-
-# special functions:
-# ------------------
-
-# the question: Who is the oldest person ?
-# return type: list of strings (name or names if there are two more with the same value)
 def get_oldest_person(table):
+    oldest_age = 9999
+    oldest = []
 
-    # your code
 
-    pass
+    for i in range(len(table)):
+        if oldest_age == int(table[i][2]):
+            oldest.append(table[i][1])
+            oldest_age = int(table[i][2])
+        elif oldest_age < int(table[i][2]):
+            oldest_age = int(table[i][2])
+            oldest = []
+            oldest.append(table[i][1])
 
+    
+    return oldest
 
 # the question: Who is the closest to the average age ?
 # return type: list of strings (name or names if there are two more with the same value)
 def get_persons_closest_to_average(table):
+    irr_age = 9999
+    closest = []
 
-    # your code
 
-    pass
+    sum_of_ages = 0
+    for i in range(len(table)):
+        sum_of_ages += int(table[i][2])
+    
+    average_age = sum_of_ages/len(table)
+
+    for i in range(len(table)):
+        if abs(average_age-irr_age) == int(table[i][2])-irr_age:
+            oldest.append(table[i][1])
+            irr_age = int(table[i][2])
+        elif abs(average_age-irr_age) < int(table[i][2])-irr_age:
+            irr_age = int(table[i][2])
+            closest = []
+            closest.append(table[i][1])
+    
+    return closest
