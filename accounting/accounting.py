5172f31042b276986f4f26f85077d71cbfe1f691
nagyrobert1990 <nagy.robert1990@gmailcom>
2017-12-12 12:10:52 +0100

    accounting.py special functions completed

--- accounting/accounting.py
+++ accounting/accounting.py
@@ -8,134 +8,53 @@ def start_module():
 
     common.submenu_of_managements(5)
 
-'''
-def id_existance(table,id_):
-
-    for i in range(len(table)):
-        if table[i][0] == id_ :
-            return True
-    
-    return False
-
-def show_table(table):
-    """
-    Display a table
-
-    Args:
-        table: list of lists to be displayed.
-
-    Returns:
-        None
-    """
-
-    # your code
-    # month: number
-# day: number
-# year: number
-# type: string (in = income, out = outcome)
-# amount: number (dollar)
-
-    ui.print_table(table,['ID','Month','Day','Year','Type','Amount'])
-
-    pass
-
-
-def add(table):
-    """
-    Asks user for input and adds it into the table.
-
-    Args:
-        table: table to add new record to
-
-    Returns:
-        Table with a new record
-    """
-
-    # your code
-
-    new_row = ui.get_inputs(['Month: ','Day: ','Year: ','Type: ','Amount: '],"Please, give me the specs.")
-    added_id = common.generate_random(table)
-    new_row.insert(0,added_id)
-    table.append(new_row)
-    data_manager.write_table_to_file("accounting/items.csv",table)
-    show_table(table)
-    ui.print_error_message('The specs are added with ID: ' + added_id)
-
-    return table
-
-
-def remove(table, id_):
-    """
-    Remove a record with a given id from the table.
-
-    Args:
-        table: table to remove a record from
-        id_ (str): id of a record to be removed
-
-    Returns:
-        Table without specified record.
-    """
-
-    # your code
-
-    for i in range(len(table)):
-        if table[i][0] == id_:
-            del(table[i])
-            break
-    
-    data_manager.write_table_to_file("accounting/items.csv",table)
-    show_table(table)
-    ui.print_error_message('The row with ID: %s has been removed.' % id_)
-
-    return table
-
-
-def update(table, id_):
-    """
-    Updates specified record in the table. Ask users for new data.
-
-    Args:
-        table: list in which record should be updated
-        id_ (str): id of a record to update
-
-    Returns:
-        table with updated record
-    """
-
-    # your code
-
-    updated_row = ui.get_inputs(['Month: ','Day: ','Year: ','Type: ','Amount: '],"Please, give me the specs.")
-    updated_row.insert(0,id_)
-    
-    for i in range(len(table)):
-        if table[i][0] == id_:
-            table.insert(i,updated_row)
-            del(table[i+1])
-            break
-
-    data_manager.write_table_to_file("accounting/items.csv",table)
-    show_table(table)
-    ui.print_error_message('The row with ID: %s has been updated.' % id_)
-
-    return table
-'''
-
-# special functions:
-# ------------------
-
-# the question: Which year has the highest profit? (profit=in-out)
-# return the answer (number)
 def which_year_max(table):
+    profit_by_year = {}
 
-    # your code
+    for line in range(len(table)):
+        in_out = [0,0]
+        if profit_by_year.get(table[line][3]) != None :
+            if table[line][4] == "in":
+                profit_by_year[table[line][3]][0] += int(table[line][5])
+            elif table[line][4] == "out":
+                profit_by_year[table[line][3]][1] += int(table[line][5])
+        else:
+            if table[line][4] == "in":
+                in_out[0] += int(table[line][5])
+            elif table[line][4] == "out":
+                in_out[1] += int(table[line][5])
+            profit_by_year[table[line][3]] = in_out
+            
+    for year in profit_by_year:
+        profit_by_year[year] = profit_by_year[year][0] - profit_by_year[year][1]
+    
+    return int(max(profit_by_year, key=profit_by_year.get))
 
-    pass
 
-
-# the question: What is the average (per item) profit in a given year? [(profit)/(items count) ]
-# return the answer (number)
 def avg_amount(table, year):
 
-    # your code
+    profit_by_year = {}
+    items_count = 0
 
-    pass
+    for line in range(len(table)):
+        in_out = [0,0]
+        if profit_by_year.get(table[line][3]) != None :
+            if table[line][4] == "in":
+                profit_by_year[table[line][3]][0] += int(table[line][5])
+            elif table[line][4] == "out":
+                profit_by_year[table[line][3]][1] += int(table[line][5])
+        else:
+            if table[line][4] == "in":
+                in_out[0] += int(table[line][5])
+            elif table[line][4] == "out":
+                in_out[1] += int(table[line][5])
+            profit_by_year[table[line][3]] = in_out
+            
+    for key in profit_by_year:
+        profit_by_year[key] = profit_by_year[key][0] - profit_by_year[key][1]
+    
+    for line in range(len(table)):
+        if int(table[line][3]) == year:
+            items_count += 1
+
+    return int(profit_by_year[year]/items_count)
