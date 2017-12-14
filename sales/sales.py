53f5107e3abc5c0260691e9101d08ff570a25d67
nagyrobert1990 <nagy.robert1990@gmailcom>
2017-12-14 13:00:08 +0100

    sales.py refactored

--- sales/sales.py
+++ sales/sales.py
@@ -5,7 +5,7 @@ import common
 
 
 def start_module():
-        
+
     common.submenu_of_managements(0)
 
 
@@ -44,36 +44,14 @@ def get_items_sold_between(table, month_from, day_from, year_from, month_to, day
 
 def get_title_by_id(id):
 
-    """
-    Reads the table with the help of the data_manager module.
-    Returns the title (str) of the item with the given id (str) on None in case of non-existing id
-    Args:
-        id (str): the id of the item
-
-    Returns:
-        str the title of the item
-    """
-
-    table = data_manager.get_table_from_file("sales.csv")
+    table = data_manager.get_table_from_file("sales/sales.csv")
     if common.is_id_exists(id) == True:
         for line in table:
             if line[0] == id:
                 return line[1]
-    
-    
+
+
 def get_title_by_id_from_table(table, id):
-
-    """
-    Returns the title (str) of the item with the given id (str) on None om case of non-existing id.
-
-    Args:
-        table (list of lists): the sales table
-        id (str): the id of the item
-
-    Returns:
-        str the title of the item
-    """
-
     if common.is_id_exists(id) == True:
         for line in table:
             if line[0] == id:
@@ -81,13 +59,6 @@ def get_title_by_id_from_table(table, id):
 
 
 def get_item_id_sold_last():
-    """
-    Reads the table with the help of the data_manager module.
-    Returns the _id_ of the item that was sold most recently.
-
-    Returns:
-        (str) the _id_ of the item that was sold most recently.
-    """
     last_sold_id = ""
     table = data_manager.get_table_from_file("sales/sales.csv")
     dates = []
@@ -108,16 +79,6 @@ def get_item_id_sold_last():
 
 
 def get_item_id_sold_last_from_table(table):
-    """
-    Returns the _id_ of the item that was sold most recently.
-
-    Args:
-        table (list of lists): the sales table
-
-    Returns:
-        (str) the _id_ of the item that was sold most recently.
-    """
-
     last_sold_id = ""
     dates = []
 
@@ -137,16 +98,6 @@ def get_item_id_sold_last_from_table(table):
 
 
 def get_item_title_sold_last_from_table(table):
-    """
-    Returns the _title_ of the item that was sold most recently.
-
-    Args:
-        table (list of lists): the sales table
-
-    Returns:
-        (str) the _title_ of the item that was sold most recently.
-    """
-
     last_sold_id = ""
     dates = []
 
@@ -166,17 +117,6 @@ def get_item_title_sold_last_from_table(table):
 
 
 def get_the_sum_of_prices(item_ids):
-    """
-    Reads the table of sales with the help of the data_manager module.
-    Returns the sum of the prices of the items in the item_ids.
-
-    Args:
-        item_ids (list of str): the ids
-
-    Returns:
-        (number) the sum of the items' prices
-    """
-
     table = data_manager.get_table_from_file("sales/sales.csv")
     sum_of_prices = 0
 
@@ -189,17 +129,6 @@ def get_the_sum_of_prices(item_ids):
 
 
 def get_the_sum_of_prices_from_table(table, item_ids):
-    """
-    Returns the sum of the prices of the items in the item_ids.
-
-    Args:
-        table (list of lists): the sales table
-        item_ids (list of str): the ids
-
-    Returns:
-        (number) the sum of the items' prices
-    """
-
     sum_of_prices = 0
 
     for ids in item_ids:
@@ -211,16 +140,6 @@ def get_the_sum_of_prices_from_table(table, item_ids):
 
 
 def get_customer_id_by_sale_id(sale_id):
-    """
-    Reads the sales table with the help of the data_manager module.
-    Returns the customer_id that belongs to the given sale_id
-    or None if no such sale_id is in the table.
-    Args:
-         sale_id (str): sale id to search for
-    Returns:
-         customer_id that belongs to the given sale id
-    """
-
     table = data_manager.get_table_from_file("sales/sales.csv")
 
     for line in table:
@@ -229,29 +148,12 @@ def get_customer_id_by_sale_id(sale_id):
 
 
 def get_customer_id_by_sale_id_from_table(table, sale_id):
-    """
-    Returns the customer_id that belongs to the given sale_id
-    or None if no such sale_id is in the table.
-    Args:
-        table: table to remove a record from
-        sale_id (str): sale id to search for
-    Returns:
-         customer_id that belongs to the given sale id
-    """
-
     for line in table:
         if sale_id == line[0]:
             return str(line[6])
 
 
 def get_all_customer_ids():
-    """
-    Reads the sales table with the help of the data_manager module.
-    Returns a set of customer_ids that are present in the table.
-    Returns:
-         set of customer_ids that are present in the table
-    """
-
     table = data_manager.get_table_from_file("sales/sales.csv")
     customer_ids = set()
 
@@ -263,14 +165,6 @@ def get_all_customer_ids():
 
 
 def get_all_customer_ids_from_table(table):
-    """
-    Returns a set of customer_ids that are present in the table.
-    Args:
-        table (list of list): the sales table
-    Returns:
-         set of customer_ids that are present in the table
-    """
-
     customer_ids = set()
 
     for line in table:
@@ -281,17 +175,6 @@ def get_all_customer_ids_from_table(table):
 
 
 def get_all_sales_ids_for_customer_ids():
-    """
-    Reads the customer-sales association table with the help of the data_manager module.
-    Returns a dictionary of (customer_id, sale_ids) where:
-        customer_id:
-        sale_ids (list): all the sales belong to the given customer
-    (one customer id belongs to only one tuple)
-    Returns:
-         (dict of (key, value): (customer_id, (list) sale_ids)) where the sale_ids list contains
-         all the sales id belong to the given customer_id
-    """
-
     table = data_manager.get_table_from_file("sales/sales.csv")
     all_customer_ids = list(get_all_customer_ids_from_table(table))
     sales_ids_for_customer_ids = {}
@@ -306,19 +189,7 @@ def get_all_sales_ids_for_customer_ids():
     return sales_ids_for_customer_ids
 
 
-def get_all_sales_ids_for_customer_ids_form_table(table):
-    """
-    Returns a dictionary of (customer_id, sale_ids) where:
-        customer_id:
-        sale_ids (list): all the sales belong to the given customer
-    (one customer id belongs to only one tuple)
-    Args:
-        table (list of list): the sales table
-    Returns:
-         (dict of (key, value): (customer_id, (list) sale_ids)) where the sale_ids list contains
-         all the sales id belong to the given customer_id
-    """
-    
+def get_all_sales_ids_for_customer_ids_form_table(table):    
     all_customer_ids = list(get_all_customer_ids_from_table(table))
     sales_ids_for_customer_ids = {}
 
@@ -333,14 +204,6 @@ def get_all_sales_ids_for_customer_ids_form_table(table):
 
 
 def get_num_of_sales_per_customer_ids():
-    """
-     Reads the customer-sales association table with the help of the data_manager module.
-     Returns a dictionary of (customer_id, num_of_sales) where:
-        customer_id:
-        num_of_sales (number): number of sales the customer made
-     Returns:
-         dict of (key, value): (customer_id (str), num_of_sales (number))
-    """
     table = data_manager.get_table_from_file("sales/sales.csv")
     all_customer_ids = list(get_all_customer_ids_from_table(table))
     sales_per_customers = {}
@@ -356,16 +219,6 @@ def get_num_of_sales_per_customer_ids():
 
 
 def get_num_of_sales_per_customer_ids_from_table(table):
-    """
-     Returns a dictionary of (customer_id, num_of_sales) where:
-        customer_id:
-        num_of_sales (number): number of sales the customer made
-     Args:
-        table (list of list): the sales table
-     Returns:
-         dict of (key, value): (customer_id (str), num_of_sales (number))
-    """
-
     all_customer_ids = list(get_all_customer_ids_from_table(table))
     sales_per_customers = {}
 
@@ -375,5 +228,5 @@ def get_num_of_sales_per_customer_ids_from_table(table):
             if ids == line[6]:
                 sales_counter += 1
         sales_per_customers[ids] = sales_counter
-    
+
     return sales_per_customers
