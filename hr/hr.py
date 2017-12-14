f08018ab64b55057ad1a536b444e846a111a4d16
nagyrobert1990 <nagy.robert1990@gmailcom>
2017-12-14 13:07:23 +0100

    hr.py refactored

--- hr/hr.py
+++ hr/hr.py
@@ -13,16 +13,15 @@ def get_oldest_person(table):
     oldest_age = 9999
     oldest = []
 
-
     for i in range(len(table)):
         if oldest_age == int(table[i][2]):
             oldest.append(table[i][1])
             oldest_age = int(table[i][2])
+
         elif oldest_age < int(table[i][2]):
             oldest_age = int(table[i][2])
             oldest = []
             oldest.append(table[i][1])
-
     
     return oldest
 
@@ -30,9 +29,8 @@ def get_oldest_person(table):
 def get_persons_closest_to_average(table):
     irr_age = 9999
     closest = []
-
-
     sum_of_ages = 0
+
     for i in range(len(table)):
         sum_of_ages += int(table[i][2])
     
@@ -42,6 +40,7 @@ def get_persons_closest_to_average(table):
         if abs(average_age-irr_age) == int(table[i][2])-irr_age:
             oldest.append(table[i][1])
             irr_age = int(table[i][2])
+            
         elif abs(average_age-irr_age) < int(table[i][2])-irr_age:
             irr_age = int(table[i][2])
             closest = []
