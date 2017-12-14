5ae692c65bbb40e0350d442866c08f0549515267
nagyrobert1990 <nagy.robert1990@gmailcom>
2017-12-14 11:05:45 +0100

    special functions in store.py are completed

--- store/store.py
+++ store/store.py
@@ -7,78 +7,27 @@ import common
 def start_module():
 
     common.submenu_of_managements(1)
-    
-'''
-def id_existance(table,id_):
-    for i in range(len(table)):
-        if table[i][0] == id_ :
-            return True
-    return False
 
 
-def show_table(table):
-    ui.print_table(table,['ID','Title','Manufacturer','Price','In Stock'])
-    pass
-
-
-def add(table):
-    new_row = ui.get_inputs(['Title: ','Manufacturer: ','Price: ','In Stock: '],"Please, give me the specs.")
-    added_id = common.generate_random(table)
-    new_row.insert(0,added_id)
-    table.append(new_row)
-    data_manager.write_table_to_file("store/games.csv",table)
-    show_table(table)
-    ui.print_error_message('The specs are added with ID: ' + added_id)
-
-    return table
-
-
-def remove(table, id_):    
-    for i in range(len(table)):
-        if table[i][0] == id_:
-            del(table[i])
-            break
-    
-    data_manager.write_table_to_file("store/games.csv",table)
-    show_table(table)
-    ui.print_error_message('The row with ID: %s has been removed.' % id_)
-
-    return table
-
-
-def update(table, id_):
-    updated_row = ui.get_inputs(['Title: ','Manufacturer: ','Price: ','In Stock: '],"Please, give me the specs.")
-    updated_row.insert(0,id_)
-    
-    for i in range(len(table)):
-        if table[i][0] == id_:
-            table.insert(i,updated_row)
-            del(table[i+1])
-            break
-
-    data_manager.write_table_to_file("store/games.csv",table)
-    show_table(table)
-    ui.print_error_message('The row with ID: %s has been updated.' % id_)
-
-    return table
-'''
-
-# special functions:
-# ------------------
-
-# the question: How many different kinds of game are available of each manufacturer?
-# return type: a dictionary with this structure: { [manufacturer] : [count] }
 def get_counts_by_manufacturers(table):
+    result = {}
 
-    # your code
+    for line in table:
+        if line[2] not in result:
+            result[line[2]] = 1
+        else:
+            result[line[2]] += 1
 
-    pass
+    return result
 
 
-# the question: What is the average amount of games in stock of a given manufacturer?
-# return type: number
 def get_average_by_manufacturer(table, manufacturer):
+    sum_of_instock = 0
+    counter = 0
 
-    # your code
-
-    pass
+    for line in table:
+        if manufacturer == line[2]:
+            counter += 1
+            sum_of_instock += int(line[4])
+    
+    return round((sum_of_instock/counter), 2)
